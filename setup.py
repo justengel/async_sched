183d58903136e84d56ec31c33bae632cfe29543b
justengel <jtengel08@gmail.com>
2020-06-20 14:48:21 -0400

    Updated requirement serial_json>=1.2.5

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
 
           # options to install extra requirements
           install_requires=[
-              'serial_json>=1.2.4',
+              'serial_json>=1.2.5',
               ],
           extras_require={
               },
